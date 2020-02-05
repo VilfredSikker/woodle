b23d22a61a67e0635c42013419f1f8247af6e604
Vilfred Sikker <vilfredsikker@gmail.com>
2020-02-05 11:17:35 +0100

    Styling for nav and toggle on logged in

--- app/src/components/navbar/navbar.tsx
+++ app/src/components/navbar/navbar.tsx
@@ -1,51 +1,45 @@
-import React, { useState, useRef } from "react";
-import { Link, Redirect } from "react-router-dom";
-import { logout, isSignedIn } from "../../utils/auth";
-import { useAppContextProvider } from "../context/app-context";
 import {
   AppBar,
-  IconButton,
-  Toolbar,
-  makeStyles,
   Button,
-  Menu,
+  ClickAwayListener,
+  Grow,
+  makeStyles,
   MenuItem,
   MenuList,
-  Popper,
   Paper,
-  Grow,
-  ClickAwayListener
-} from "@material-ui/core";
-import MenuIcon from "@material-ui/icons/Menu";
+  Popper,
+  Toolbar
+} from "@material-ui/core"
+import MenuIcon from "@material-ui/icons/Menu"
+import React, { useRef, useState } from "react"
+import { Link } from "react-router-dom"
+import { isSignedIn, logout } from "../../utils/auth"
+import { useAppContextProvider } from "../context/app-context"
 
 const useStyles = makeStyles({
   container: {
     background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
     color: "white",
-    paddingBottom: "20",
     position: "relative"
+  },
+  content: {
+    display: "flex",
+    justifyContent: "space-between"
   }
-});
+})
 
 const Navbar = () => {
-  const { jwtToken, theme, lang, updateAppContext } = useAppContextProvider();
-  const styles = useStyles();
-  const loggedIn = isSignedIn(jwtToken);
-  const content = { message: "", login: true };
-  const [ openBurger, setOpenBurger] = useState(false)
+  const { jwtToken, theme, lang, updateAppContext } = useAppContextProvider()
+  const styles = useStyles()
+  const loggedIn = isSignedIn(jwtToken)
+  const [openBurger, setOpenBurger] = useState(false)
   const burgerRef = useRef<HTMLButtonElement>(null)
-
-  if (loggedIn) {
-    content.message = `Hello`;
-  } else {
-    content.message = "You are not logged in";
-  }
 
   function handleToggleBurger() {
     setOpenBurger(prev => !prev)
   }
 
-  function handleCloseBurger(event : any ) {
+  function handleCloseBurger(event: any) {
     const current = burgerRef.current
 
     if (current && current.contains(event.target)) {
@@ -55,67 +49,62 @@ const Navbar = () => {
     setOpenBurger(false)
   }
 
+  const contentLoggedIn = (
+    <Toolbar className={styles.content}>
+      <Button ref={burgerRef} onClick={handleToggleBurger}>
+        <MenuIcon />
+      </Button>
+      <Popper
+        open={openBurger}
+        anchorEl={burgerRef.current}
+        role={undefined}
+        transition
+        disablePortal
+      >
+        <Grow
+          in={openBurger}
+          timeout={150}
+          style={{ transformOrigin: "center bottom" }}
+        >
+          <Paper>
+            <ClickAwayListener onClickAway={handleCloseBurger}>
+              <MenuList autoFocusItem={openBurger} id="menu-list-grow">
+                <MenuItem onClick={handleCloseBurger}>Profile</MenuItem>
+                <MenuItem onClick={handleCloseBurger}>My account</MenuItem>
+                <MenuItem onClick={handleCloseBurger}>Logout</MenuItem>
+              </MenuList>
+            </ClickAwayListener>
+          </Paper>
+        </Grow>
+      </Popper>
+      <Button
+        onClick={e => {
+          updateAppContext({ jwtToken: "", theme, lang })
+          logout()
+        }}
+      >
+        <Link to="/">Logout</Link>
+      </Button>
+    </Toolbar>
+  )
+
+  const contentSignedOut = (
+    <Toolbar className={styles.content}>
+      <span>Welcome to woodle</span>
+      <Button ref={burgerRef} onClick={handleToggleBurger}>
+        <Link to="/login">Login</Link>
+      </Button>
+      <Button ref={burgerRef} onClick={handleToggleBurger}>
+        <Link to="/sign-up">Sign Up</Link>{" "}
+      </Button>
+    </Toolbar>
+  )
+
   return (
     <AppBar className={styles.container}>
-      <Toolbar>
-        <Button 
-          ref={burgerRef}
-          onClick={(handleToggleBurger)}
-        >
-          <MenuIcon />
-        </Button>
-        <Popper open={openBurger} anchorEl={burgerRef.current} role={undefined} transition disablePortal>
-            <Grow
-              in={openBurger}
-              timeout={150}
-              style={{ transformOrigin: 'center bottom' }}
-            >
-              <Paper>
-                <ClickAwayListener onClickAway={handleCloseBurger}>
-                  <MenuList autoFocusItem={openBurger} id="menu-list-grow" >
-                    <MenuItem onClick={handleCloseBurger}>Profile</MenuItem>
-                    <MenuItem onClick={handleCloseBurger}>My account</MenuItem>
-                    <MenuItem onClick={handleCloseBurger}>Logout</MenuItem>
-                  </MenuList> 
-                </ClickAwayListener>
-              </Paper>
-            </Grow>
-        </Popper>
-      </Toolbar>
+      {loggedIn ? contentLoggedIn : contentSignedOut}
     </AppBar>
-  );
-  // return (
-  //   <div
-  //     style={{
-  //       display: "flex",
-  //       flex: "1",
-  //       justifyContent: "space-between",
-  //       borderBottom: "1px solid #d1c1e0"
-  //     }}
-  //   >
-  //     <span>{content.message}</span>
-  //     <nav>
-  //       <Link to="/">Welcome Page</Link>
-  //       {` `}
-  //       <Link to="/login">Login</Link>
-  //       <Link to="/sign-up">Sign Up</Link>
-  //       <Link to="/app/profile">Profile</Link>
-  //       {` `}
-  //       {loggedIn ? (
-  //         <button
-  //           onClick={e => {
-  //             updateAppContext({jwtToken: "", theme, lang})
-  //             logout()
-  //           }}
-  //         >
-  //           <Link to="/" >Logout</Link>
-  //         </button>
-  //       ) : (
-  //         <Link to="/login">Sign In</Link>
-  //       )}
-  //     </nav>
-  //   </div>
-  // );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
--- app/src/utils/auth.js
+++ app/src/utils/auth.js
@@ -1,36 +1,40 @@
-import { Auth } from 'aws-amplify'
+import { Auth } from "aws-amplify"
 
 export function isSignedIn(jwtToken) {
-    return (jwtToken === "" || jwtToken === "null") ? false : true
+  const loggedIn = jwtToken === "" || jwtToken === "null" ? false : true
+  console.log("is signed in? ", jwtToken, loggedIn)
+  return loggedIn
 }
 
 export function saveJwtTokenToStorage(jwtToken) {
-    sessionStorage.setItem('jwtToken', jwtToken)
+  sessionStorage.setItem("jwtToken", jwtToken)
 }
 
 export function getJwtTokenFromStorage() {
-    const token = sessionStorage.getItem('jwtToken')
-    console.log("returning token: ", token)
-    return token
+  const token = sessionStorage.getItem("jwtToken")
+  console.log("returning token: ", token)
+  return token
 }
 
 export async function getUser() {
-    console.log("Getting user")
-    let currentUser;
+  console.log("Getting user")
+  let currentUser
 
-    await Auth.currentAuthenticatedUser()
-        .then(user => currentUser=user)
-        .catch(err => {
-            currentUser = undefined
-            console.log("getUser error: ", err)
-        })
+  await Auth.currentAuthenticatedUser()
+    .then(user => (currentUser = user))
+    .catch(err => {
+      currentUser = undefined
+      console.log("getUser error: ", err)
+    })
 
-    return currentUser
+  return currentUser
 }
 
 export const logout = () => {
-    console.log("Signing out")
-    saveJwtTokenToStorage("")
-    
-    Auth.signOut().then(data => console.log("Logout successful, data: ", data)).catch(err => console.log("Sign out failed: ", err))
+  console.log("Signing out")
+  saveJwtTokenToStorage("")
+
+  Auth.signOut()
+    .then(data => console.log("Logout successful, data: ", data))
+    .catch(err => console.log("Sign out failed: ", err))
 }
