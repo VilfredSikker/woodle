27be158b1662c6367449dbd4ee3ebfa72098f608
Vilfred Sikker <vilfredsikker@gmail.com>
2020-02-11 17:38:17 +0100

    styling of button

--- app/src/components/basics/button/button.tsx
+++ app/src/components/basics/button/button.tsx
@@ -1,10 +1,23 @@
 import React, { ReactNode } from "react"
 import Button from "@material-ui/core/Button"
+import { withStyles, makeStyles } from "@material-ui/core"
 
-interface StyledButtonProps {}
+const useStyles = makeStyles({
+  button: {
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    borderRadius: 3,
+    border: 0,
+    color: "white"
+  }
+})
 
 const StyledButton = (props: any, children: any) => {
-  return <Button {...props}>{props.children}</Button>
+  const styles = useStyles()
+  return (
+    <Button className={styles.button} {...props}>
+      {props.children}
+    </Button>
+  )
 }
 
 export default StyledButton
